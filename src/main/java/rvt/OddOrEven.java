af44565f49bcff4696ddad8fcf2a558b2971a991
Aleksandrs Klimanovs <a240853ak@rvt.lv>
2025-09-18 12:45:16 +0300

    Odd or even

---
+++ src/main/java/rvt/OddOrEven.java
@@ -0,0 +1,18 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        Integer a = Integer.valueOf(scanner.nextLine());
+
+        if (a % 2 == 0) {
+            System.out.println("Number " + a + " is even.");
+        } else {
+            System.out.println("Number " + a + " is odd.");
+        }
+    }
+}
