0c1f44db588c85d707e093831bab29f003b79e13
Aleksandrs Klimanovs <a240853ak@rvt.lv>
2025-09-18 12:34:55 +0300

    Average of three numbers

---
+++ src/main/java/rvt/Avarage.java
@@ -0,0 +1,20 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Avarage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        Integer a = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        Integer b = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        Integer c = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The average is " + ((a + b + c) / 3.0));
+    }
+}
