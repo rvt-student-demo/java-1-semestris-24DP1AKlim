16415e805318ffe5f2c2a357a5354a640405dcf7
24DP1Aklim <a240853ak@rvt.lv>
2025-09-19 10:39:47 +0300

    Password

---
+++ src/main/java/rvt/Password.java
@@ -0,0 +1,20 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Password?");
+        String pass = scanner.nextLine();
+
+        if (pass == "Caput Draconis") {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you.");
+        }
+
+        scanner.close();
+    }
+}
