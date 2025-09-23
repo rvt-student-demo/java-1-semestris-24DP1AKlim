96ed2a4cc6e160122e9bd06d75cac0a0a52b07ad
24DP1Aklim <a240853ak@rvt.lv>
2025-09-23 14:02:06 +0300

    Detalas

---
+++ src/main/java/rvt/Detalas.java
@@ -0,0 +1,37 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Detalas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int skruvesPrice = 5;
+        final int uzgrieznesPrice = 3;
+        final int paplaknesPrice = 1;
+
+        System.out.println("Skruvju skaits?");
+        Integer skruves = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Uzgrieznu skaits?");
+        Integer uzgrieznes = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Paplaksnu skaits?");
+        Integer paplaknes = Integer.valueOf(scanner.nextLine());
+
+        if (uzgrieznes < skruves && paplaknes < 2 * skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz uzgrieznu un paplaksnu");
+        } else if (uzgrieznes < skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz uzgrieznu");
+        } else if (paplaknes < 2 * skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz paplaksnu");
+        } else {
+            System.out.println("Pasutijums ir kartiba.");
+        }
+
+        System.out.println(
+                "Kopeja cena: " + (skruves * skruvesPrice + uzgrieznes * uzgrieznesPrice + paplaknes * paplaknesPrice));
+
+        scanner.close();
+    }
+}
