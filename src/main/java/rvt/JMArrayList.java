064bad978d4605b0835c5e425b472a252bfecf1e
24DP1Aklim <a240853ak@rvt.lv>
2025-10-17 10:53:57 +0300

    JMArrayList start

---
+++ src/main/java/rvt/JMArrayList.java
@@ -0,0 +1,69 @@
+package rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JMArrayList {
+    public static void main(String[] args) {
+        listSize();
+    }
+
+    public static void onlyTheseNumbers() {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == -1)
+                break;
+
+            list.add(input);
+        }
+
+        System.out.print("From where? ");
+        int from = Integer.valueOf(scanner.nextLine());
+        System.out.print("To where? ");
+        int to = Integer.valueOf(scanner.nextLine());
+
+        for (; from <= to; from++) {
+            System.out.println(list.get(from));
+        }
+    }
+
+    public static void listSize() {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty())
+                break;
+
+            list.add(input);
+        }
+
+        System.out.println("In total: " + list.size());
+    }
+
+    public static void onTheList() {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.isEmpty())
+                break;
+
+            list.add(input);
+        }
+
+        System.out.print("Search for? ");
+        String search = scanner.nextLine();
+    }
+}
