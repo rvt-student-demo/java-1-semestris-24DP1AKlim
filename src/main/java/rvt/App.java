99d7803b6cb1413493bbbb80a3d943c5acf42d03
24DP1Aklim <a240853ak@rvt.lv>
2025-10-28 14:33:31 +0200

    statistics

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,4 +1,33 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Statistics stats = new Statistics();
+        Statistics statsEven = new Statistics();
+        Statistics statsOdd = new Statistics();
+
+        while (true) {
+            int num = scanner.nextInt();
+
+            if (num == -1)
+                break;
+
+            if (num % 2 == 0)
+                statsEven.addNumber(num);
+            else
+                statsOdd.addNumber(num);
+
+            stats.addNumber(num);
+        }
+
+        System.out.println("Sum: " + stats.sum());
+        System.out.println("Sum of even numbers: " + statsEven.sum());
+        System.out.println("Sum of odd numbers: " + statsOdd.sum());
+
+        scanner.close();
+    }
 }
---
+++ src/main/java/rvt/Statistics.java
@@ -0,0 +1,28 @@
+package rvt;
+
+import java.util.ArrayList;
+
+public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics() {
+    }
+
+    public void addNumber(int number) {
+        sum += number;
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        return (double) sum / count;
+    }
+}
