b1647a00701180a46be9aa28af5a60bb9b05a1fa
Aleksandrs Klimanovs <a240853ak@rvt.lv>
2025-11-11 19:09:33 +0200

    products

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,27 +1,96 @@
 package rvt;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
+        Shop shop = new Shop();
 
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
+        shop.add(new Product("Keyboard", 54.99));
+        shop.add(new FoodProduct("Banana", 2, "11.11.2025"));
+        shop.add(new DiscountProduct("Book", 12.30, 50));
 
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
+        Scanner scanner = new Scanner(System.in);
 
-        paulsCard.addMoney(20);
-        mattsCard.eatHeartily();
+        System.out.print("Ievadiet produkta nosaukumu: ");
+        String name = scanner.nextLine();
 
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
+        System.out.print("Ievadiet produkta cenu: ");
+        shop.add(new Product(name, scanner.nextDouble()));
 
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
+        for (int i = 0; i < shop.size(); i++) {
+            System.out.println(shop.get(i));
+            System.out.println(shop.get(i).getFinalPrice());
+        }
+        System.out.println(shop.sumOfPrices());
 
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
+        scanner.close();
+    }
+}
+
+class Shop extends ArrayList<Product> {
+    public double sumOfPrices() {
+        double sum = 0;
+
+        for (int i = 0; i < this.size(); i++) {
+            sum += this.get(i).getFinalPrice();
+        }
+        return sum;
+    }
+}
+
+class Product {
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Price: " + price;
+    }
+
+    public double getFinalPrice() {
+        return price;
+    }
+}
+
+class FoodProduct extends Product {
+    String expiryDate;
+
+    public FoodProduct(String name, double price, String expiryDate) {
+        // super šajā piemerā ir Product (parent class)
+        super(name, price);
+        this.expiryDate = expiryDate;
+    }
+
+    // Override, lai modificētu oriģinalo toString() metodi
+    @Override
+    public String toString() {
+        return super.toString() + ", Expiry date: " + expiryDate;
+    }
+}
+
+class DiscountProduct extends Product {
+    int discountPercent;
+
+    public DiscountProduct(String name, double price, int discountPercent) {
+        // super šajā piemerā ir Product (parent class)
+        super(name, price);
+        this.discountPercent = discountPercent;
+    }
+
+    @Override
+    public double getFinalPrice() {
+        return price - (price * discountPercent / 100);
+    }
+
+    // Override, lai modificētu oriģinalo toString() metodi
+    @Override
+    public String toString() {
+        return super.toString() + ", discount Percent: " + discountPercent;
     }
 }
